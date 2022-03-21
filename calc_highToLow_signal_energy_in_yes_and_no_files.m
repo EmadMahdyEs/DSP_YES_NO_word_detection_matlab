7b03c035ed37bafa681c1208c26ffb695a1f6a02
EmadMahdyEs <emad.mahdy.es@gmail.com>
2022-03-22 01:32:30 +0200

    edit the way to read sound waves from directory

--- calc_highToLow_signal_energy_in_yes_and_no_files.m
+++ calc_highToLow_signal_energy_in_yes_and_no_files.m
@@ -1,13 +1,16 @@
 function [R_yes,R_no] = calc_highToLow_signal_energy_in_yes_and_no_files()
-    for i=1:1:24
-        
-%         yes_file_name= strcat('./ProjectSounds/GoodYes/y',int2str(i),'.wav');
-%         no_file_name= strcat('./ProjectSounds/GoodNo/n',int2str(i),'.wav');
-        yes_file_name= strcat('./ProjectSounds_noSine/GoodYes/y',int2str(i),'.wav');
-        no_file_name= strcat('./ProjectSounds_noSine/GoodNo/n',int2str(i),'.wav');
-        [y_yes,Fs] = audioread(yes_file_name);
+    files_Y = dir('./ProjectSounds_noSine/GoodYes/*.wav') ; 
+    files_N = dir('./ProjectSounds_noSine/GoodNo/*.wav') ; 
+
+    for i=1:1:length(files_Y)
+        current_file=strcat('./ProjectSounds_noSine/GoodYes/', files_Y(i).name);
+        [y_yes,Fs] = audioread(current_file);
         R_yes(i) = highToLow_signal_energy_ratio_func(y_yes);
-        [y_no,Fs] = audioread(no_file_name);
+    end
+    
+    for i=1:1:length(files_N)
+        current_file=strcat('./ProjectSounds_noSine/GoodNo/', files_N(i).name);
+        [y_no,Fs] = audioread(current_file);
         R_no(i) =  highToLow_signal_energy_ratio_func(y_no);
     end
 end
--- main_project.m
+++ main_project.m
@@ -1,25 +1,27 @@
 clear;
 clc;
 
+%[R_yes_vect,R_no_vect] = calc_highToLow_signal_energy_in_yes_and_no_files();
+%R_yes_mean = mean(R_yes_vect );
+%R_no_mean = mean(R_no_vect );
+%yes_to_no_R=R_yes_mean/R_no_mean;
 
-[R_yes_vect,R_no_vect] = calc_highToLow_signal_energy_in_yes_and_no_files();
-R_yes_mean = mean(R_yes_vect );
-R_no_mean = mean(R_no_vect );
-yes_to_no_R=R_yes_mean/R_no_mean;
-
-min_of_Y=min(R_yes_vect)
-max_of_N=max(R_no_vect)
-
-Threshold=(min_of_Y+max_of_N)/2;
-
-R_Others = calc_energies_in_Others_files();
+%min_of_Y=min(R_yes_vect);
+%max_of_N=max(R_no_vect);
 
 
 
-[check_Y,check_N]=checka_all_good_yes_and_no_files()
+%R_Others = calc_energies_in_Others_files();
+
+%[check_Y,check_N]=checka_all_good_yes_and_no_files();
 
 %E_others=calc_energies_in_Others_files()
 
-%check_yes_or_no('./ProjectSounds/Others/n2.wav')
-%check_yes_or_no('./ProjectSounds/Others/y1.wav')
+T=.01;
+
+check_yes_or_no('./ProjectSounds_noSine/GoodYes/y7.wav',T)
+check_yes_or_no('./ProjectSounds_noSine/GoodNo/n7.wav',T)
+check_yes_or_no('./ProjectSounds_noSine/Other/n8.wav',T)
+
+
 
